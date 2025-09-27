c071cafa5adb698e5ee97a70794d7fd9b92232f5
Sebastian Karl <sebastian.karl08@htl-salzburg.ac.at>
2025-09-27 09:41:51 +0200

    Add command /freeze for Clientside package on Rage:Unofficial Server

--- rage_freeze_player.cs
+++ rage_freeze_player.cs
@@ -6,7 +6,12 @@ namespace FreezePlayer
 
     public class Freeze : Events.Script
     {
-        
+
+        public Freeze()
+        {
+            Events.Add("FreezePlayer", PlayerFreeze);
+        }
+
     }
 
 }
